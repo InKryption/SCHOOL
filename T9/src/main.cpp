4be5634c8561b64e11d9ab26a44a737167935d6b
InK <inkryption07@gmail.com>
2020-12-02 02:45:26 +0100

    Progress on the cmd struct matters.

--- T9/src/main.cpp
+++ T9/src/main.cpp
@@ -1,19 +1,44 @@
 #include "main.hpp"
 
 using std::string;
+using std::vector;
 using std::unordered_map;
 
 
 
 int main() {
 	
-	std::string raw_buff;
+	unordered_map<string, ink::CMD> cmd;
+	cmd.emplace(
+		ink::CMD("list", 
+			[]() -> ink::FCallback {
+				
+				printf("help - See this menu.\n");
+				
+				return ink::FCallback::IGNORE;
+			}
+		)
+	);
+	
+	string raw_buff;
+	vector<string> formatted_buff;
 	
 	while (true) {
+		
 		raw_buff = ink::prompt_input();
 		if (raw_buff == "__exit__") break; // Temp exit portal
 		
+		formatted_buff = ink::split_tokens(raw_buff);
 		
+		// Ensure that there are no more than two args.
+		if (formatted_buff.size() > 2 || formatted_buff.size() == 0) {
+			printf("Invalid number of tokens. Use 'help' to view valid commands.");
+			continue;
+		}
+		
+		ink::FCallback fcallback = cmd[formatted_buff[0]](formatted_buff[1]);
+		
+		if ( fcallback == ink::FCallback::BREAK ) break;
 		
 	}
 	
--- T9/src/main.hpp
+++ T9/src/main.hpp
@@ -1,3 +1,4 @@
 #include "common.hpp"
 #include "parse/prompt_input.hpp"
 #include "parse/split_tokens.hpp"
+#include "cmd_struct/cmd.hpp"
